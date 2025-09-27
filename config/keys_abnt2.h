c1e3b4ef5b4341e823d90ddc0e151a6e42650806
Pedro Calderon] <pedro@lexter.ai>
2025-09-26 23:15:55 -0300

    Keymap ABNT2

---
+++ config/keys_abnt2.h
@@ -0,0 +1,84 @@
+
+// Aliases
+#define BR_QUOT GRAVE            // '
+#define BR_1    N1               // 1
+#define BR_2    N2               // 2
+#define BR_3    N3               // 3
+#define BR_4    N4               // 4
+#define BR_5    N5               // 5
+#define BR_6    N6               // 6
+#define BR_7    N7               // 7
+#define BR_8    N8               // 8
+#define BR_9    N9               // 9
+#define BR_0    N0               // 0
+#define BR_MINS MINUS            // -
+#define BR_EQL  EQUAL            // =
+#define BR_Q    Q                // Q
+#define BR_W    W                // W
+#define BR_E    E                // E
+#define BR_R    R                // R
+#define BR_T    T                // T
+#define BR_Y    Y                // Y
+#define BR_U    U                // U
+#define BR_I    I                // I
+#define BR_O    O                // O
+#define BR_P    P                // P
+#define BR_ACUT LEFT_BRACE       // ´ (dead)
+#define BR_LBRC RIGHT_BRACE      // [
+#define BR_A    A                // A
+#define BR_S    S                // S
+#define BR_D    D                // D
+#define BR_F    F                // F
+#define BR_G    G                // G
+#define BR_H    H                // H
+#define BR_J    J                // J
+#define BR_K    K                // K
+#define BR_L    L                // L
+#define BR_CCED SEMICOLON        // Ç
+#define BR_TILD SINGLE_QUOTE     // ~ (dead)
+#define BR_RBRC BACKSLASH        // ]
+#define BR_BSLS NON_US_BACKSLASH // (backslash)
+#define BR_Z    Z                // Z
+#define BR_X    X                // X
+#define BR_C    C                // C
+#define BR_V    V                // V
+#define BR_B    B                // B
+#define BR_N    N                // N
+#define BR_M    M                // M
+#define BR_COMM COMMA            // ,
+#define BR_DOT  PERIOD           // .
+#define BR_SEMICOLON SLASH       // ;
+#define BR_SLSH INT1             // /
+#define BR_DQUO S(BR_QUOT)       // "
+#define BR_EXLM S(BR_1)          // !
+#define BR_AT   S(BR_2)          // @
+#define BR_HASH S(BR_3)          // #
+#define BR_DLR  S(BR_4)          // $
+#define BR_PERC S(BR_5)          // %
+#define BR_DIAE S(BR_6)          // ¨ (dead)
+#define BR_AMPR S(BR_7)          // &
+#define BR_ASTR S(BR_8)          // *
+#define BR_LPRN S(BR_9)          // (
+#define BR_RPRN S(BR_0)          // )
+#define BR_UNDS S(BR_MINS)       // _
+#define BR_PLUS S(BR_EQL)        // +
+#define BR_GRV  S(BR_ACUT)       // ` (dead)
+#define BR_LCBR S(BR_LBRC)       // {
+#define BR_CIRC S(BR_TILD)       // ^ (dead)
+#define BR_RCBR S(BR_RBRC)       // }
+#define BR_PIPE S(BR_BSLS)       // |
+#define BR_LABK S(BR_COMM)       // <
+#define BR_RABK S(BR_DOT)        // >
+#define BR_COLN S(BR_SEMICOLON)  // :
+#define BR_QUES S(BR_SLSH)       // ?
+#define BR_SUP1 ALGR(BR_1)       // ¹
+#define BR_SUP2 ALGR(BR_2)       // ²
+#define BR_SUP3 ALGR(BR_3)       // ³
+#define BR_PND  ALGR(BR_4)       // £
+#define BR_CENT ALGR(BR_5)       // ¢
+#define BR_NOT  ALGR(BR_6)       // ¬
+#define BR_SECT ALGR(BR_EQL)     // §
+#define BR_DEG  ALGR(BR_E)       // °
+#define BR_FORD ALGR(BR_LBRC)    // ª
+#define BR_MORD ALGR(BR_RBRC)    // º
+#define BR_CRUZ ALGR(BR_C)       // ₢
